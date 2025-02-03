5e9a25fc99fe89f5728b3f6213cac77805580021
Caldewood Alikula <alikulaowen@gmail.com>
2025-02-03 16:20:19 +0300

    invalid command

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,18 @@ use std::io::{self, Write};
 
 fn main() {
     // Uncomment this block to pass the first stage
-    loop{
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-     // treating all input as invalid command for now
-    println!("{}: command not found", input.trim());
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        // treating all input as invalid command for now
+        println!("{}: command not found", input.trim());
+        if input.trim() == "exit 0" {
+            break;
+        };
     }
 }
