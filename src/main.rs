01e5d4792a1e9f1704271d1322ab8790f6536b73
Caldewood Alikula <alikulaowen@gmail.com>
2025-02-04 06:10:04 +0300

    more type command

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ fn main() {
                     "echo" => println!("echo is a shell builtin"),
                     "type" => println!("type is a shell builtin"),
                     "exit" => println!("exit is a shell builtin"),
-                    _ => println!("{}: command not found", phrase),
+                    _ => println!("{}: not found", phrase),
                 }
             }
             input => println!("{}: command not found", input),
