72e8b1cac78907ef4e6f44c2983f80a8d9e2093b
Caldewood Alikula <alikulaowen@gmail.com>
2025-02-04 06:08:30 +0300

    more type command

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,16 @@ fn main() {
         match input.trim() {
             "exit 0" => exit(0),
             input if input.starts_with("echo ") => println!("{}", &input[5..]),
+            input if input.starts_with("type") =>{
+                let mut input = input.split_whitespace();
+                input.next();
+                let phrase = input.next().unwrap();
+                match phrase {
+                    "echo" => println!("echo is a shell builtin"),
+                    "type" => println!("type is a shell builtin"),
+                    _ => println!("{}: command not found", phrase),
+                }
+            }
             input => println!("{}: command not found", input),
         };
 
